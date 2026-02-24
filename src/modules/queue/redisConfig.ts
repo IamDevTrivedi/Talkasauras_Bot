a970ff84a0c3cb2bb8b13425a9d9bddd153abc2e
IamDevTrivedi <devtrivedi22082005@gmail.com>
2026-02-24 22:25:41 +0530

    fix: remove LOCAL_REDIS configuration from redisConfig

--- src/modules/queue/redisConfig.ts
+++ src/modules/queue/redisConfig.ts
@@ -1,14 +1,9 @@
 import { env } from "@/config/env.js";
 import { ConnectionOptions } from "bullmq";
 
-export const redisConfig: ConnectionOptions = env.LOCAL_REDIS
-    ? {
-          host: "localhost",
-          port: 6379,
-      }
-    : {
-          host: env.REDIS_HOST,
-          port: env.REDIS_PORT,
-          username: env.REDIS_USERNAME,
-          password: env.REDIS_PASSWORD,
-      };
+export const redisConfig: ConnectionOptions = {
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+    username: env.REDIS_USERNAME,
+    password: env.REDIS_PASSWORD,
+};
